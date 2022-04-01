2ce409ea5793776421b1322640f0ae86d0bb4872
Ori Naveh <orinaveh1@gmail.com>
2022-04-01 19:06:43 +0300

    wip: basic mui configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,39 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import { blue, red } from '@mui/material/colors';
+import type { AppProps } from 'next/app';
+import { createTheme, CssBaseline, ThemeProvider, responsiveFontSizes } from '@mui/material';
+
+let theme = createTheme({
+  typography: {
+    fontFamily: ['Arial', 'sans-serif'].join(','),
+    fontSize: 12
+  },
+  palette: {
+    primary: {
+      main: blue[300]
+    },
+    secondary: {
+      main: red[500]
+    }
+  },
+  components: {
+    MuiButton: {
+      defaultProps: {
+        variant: 'contained'
+      }
+    }
+  }
+});
+
+theme = responsiveFontSizes(theme);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+      <CssBaseline />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
