d1fc5b81dd8e80de8a778fc9fc0fe35cc0a823e8
Ori Naveh <orinaveh1@gmail.com>
2022-04-29 19:24:03 +0300

    feat: allow edit cron string directly

--- pages/cron/index.tsx
+++ pages/cron/index.tsx
@@ -4,14 +4,20 @@ import CronTester from 'react-js-cron';
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
 import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import IconButton from '@mui/material/IconButton';
+import CopyAll from '@mui/icons-material/CopyAll';
 import Grid from '@mui/material/Grid';
 import 'antd/lib/button/style/index.css';
 import 'antd/lib/select/style/index.css';
 
 import cronStyles from './index.module.scss';
+import useCopyToClipboard from '../../hooks/useCopyToClipboard';
 
 const Cron: NextPage = () => {
   const [value, setValue] = useState('');
+
+  const copyCron = useCopyToClipboard(value);
 
   return (
     <div className={styles.container}>
@@ -29,10 +35,20 @@ const Cron: NextPage = () => {
         <Grid item xs={16}>
           <CronTester className={cronStyles.cron} value={value} setValue={setValue} />
         </Grid>
-        <Grid item xs={16}>
-          <Typography sx={{ textAlign: 'center' }} variant="h3">
-            Result: {value}
-          </Typography>
+        <Grid container justifyContent="center" item xs={16}>
+          <TextField
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            sx={{ textAlign: 'center' }}
+            label="Result"
+            InputProps={{
+              endAdornment: (
+                <IconButton onClick={copyCron}>
+                  <CopyAll />
+                </IconButton>
+              )
+            }}
+          />
         </Grid>
       </Grid>
     </div>
